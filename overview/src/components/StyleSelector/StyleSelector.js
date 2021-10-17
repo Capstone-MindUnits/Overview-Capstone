627918d18fadf8e8dfaa087072844a8c99836c8c
mnawa3 <fouratir16@gmail.com>
2021-10-17 13:00:27 +0100

    to merge

--- overview/src/components/StyleSelector/StyleSelector.js
+++ overview/src/components/StyleSelector/StyleSelector.js
@@ -1,9 +1,9 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { styleAsync, selectStyle, thisstyle, currentStyle,dataAsync, selectProduct } from '../../feature/ProductSlice';
+import { styleAsync, selectStyle, thisstyle, currentStyle, dataAsync, selectProduct } from '../../feature/ProductSlice';
 
 
-function StyleSelector({style , styles}) {
+function StyleSelector({ style, styles }) {
 
     console.log(style)
     if (style !== null) {
@@ -11,16 +11,19 @@ function StyleSelector({style , styles}) {
         return (
             <div className='text-left'>
                 <p><strong> STYLE</strong> {'>'} {style.name} </p>
-                <div className='flex '>
-                    {styles.results.map((el, key) =>
-                        <div   value={el.style_id} key={key} className="m-2 border border-black testc rounded-full h-16 w-16  justify-center..."></div>
-                    )}
-
-                    {/* <div className="m-2 border border-black testc rounded-full h-16 w-16  justify-center..."></div>
-            <div className="m-2 border border-black testc rounded-full h-16 w-16  justify-center..."></div>
-            <div className="m-2 border border-black testc rounded-full h-16 w-16  justify-center..."></div> */}
+                <div className='flex flex-row '>
+                    <div className="m-2 border border-black testc rounded-full h-16 w-16  justify-center..."></div>
+                    <div className="m-2 border border-black testc rounded-full h-16 w-16  justify-center..."></div>
+                    <div className="m-2 border border-black testc rounded-full h-16 w-16  justify-center..."></div>
+                    <div className="m-2 border border-black testc rounded-full h-16 w-16  justify-center..."></div>
                 </div>
-                
+                <div className='flex flex-row '>
+                    <div className="m-2 border border-black testc rounded-full h-16 w-16  justify-center..."></div>
+                    <div className="m-2 border border-black testc rounded-full h-16 w-16  justify-center..."></div>
+                    <div className="m-2 border border-black testc rounded-full h-16 w-16  justify-center..."></div>
+                    <div className="m-2 border border-black testc rounded-full h-16 w-16  justify-center..."></div>
+                </div>
+
             </div>
         )
     }
