e9c07a0a60b96a0a6f1f0babf3a2c7515dfba469
mnawa3 <fouratir16@gmail.com>
2021-10-18 14:03:55 +0100

    last version

--- src/OverView/components/StyleSelector/StyleSelector.js
+++ src/OverView/components/StyleSelector/StyleSelector.js
@@ -12,16 +12,15 @@ function StyleSelector({ styles }) {
             <div className='text-left'>
                 <p><strong> STYLE</strong> {'>'} {styles.results[0].name} </p>
                 <div className='flex flex-row '>
-                    <div className="m-2 border border-black testc rounded-full h-16 w-16  justify-center..."></div>
-                    <div className="m-2 border border-black testc rounded-full h-16 w-16  justify-center..."></div>
-                    <div className="m-2 border border-black testc rounded-full h-16 w-16  justify-center..."></div>
-                    <div className="m-2 border border-black testc rounded-full h-16 w-16  justify-center..."></div>
+                    <div className="bg-gray-900 m-2 border border-black  rounded-full h-16 w-16  justify-center..."></div>
+                    <div className=" bg-yellow-900 m-2 border border-black  rounded-full h-16 w-16  justify-center..."></div>
+                    <div className="bg-green-900 m-2 border border-black  rounded-full h-16 w-16  justify-center..."></div>
+                    <div className="bg-blue-900 m-2 border border-black  rounded-full h-16 w-16  justify-center..."></div>
                 </div>
                 <div className='flex flex-row '>
-                    <div className="m-2 border border-black testc rounded-full h-16 w-16  justify-center..."></div>
-                    <div className="m-2 border border-black testc rounded-full h-16 w-16  justify-center..."></div>
-                    <div className="m-2 border border-black testc rounded-full h-16 w-16  justify-center..."></div>
-                    <div className="m-2 border border-black testc rounded-full h-16 w-16  justify-center..."></div>
+                    <div className="bg-pink-900 m-2 border border-black  rounded-full h-16 w-16  justify-center..."></div>
+                    <div className="m-2 border border-black  rounded-full h-16 w-16  justify-center..."></div>
+                    <div className=" bg-black m-2 border border-black  rounded-full h-16 w-16  justify-center..."></div>
                 </div>
 
             </div>
