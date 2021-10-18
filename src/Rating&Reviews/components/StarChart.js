18f592ec5fd689fe02a9ba660d290ab4edda16a5
Imad el Ayoubi <imadelayoubi07@gmail.com>
2021-10-18 13:14:30 +0100

    progress bar dynamic

--- src/Rating&Reviews/components/StarChart.js
+++ src/Rating&Reviews/components/StarChart.js
@@ -8,9 +8,14 @@ function StarChart({ reviewData }) {
     } else {
 
         var percentage;
+
         var count = 0
         var percentageArray = []
         var ratingArray = []
+
+
+        // chartProgress = (object[testArray[i]] / testArray.length) * 100
+
         for (var i = 0; i < reviewData.length; i++) {
             percentageArray.push(reviewData[i].recommend)
             ratingArray.push(reviewData[i].rating)
@@ -20,6 +25,35 @@ function StarChart({ reviewData }) {
             percentage = (count / percentageArray.length) * 100
         }
 
+        var testArray = ratingArray.sort((a, b) => {
+            return b - a
+        })
+        var object = {
+            5: 0,
+            4: 0,
+            3: 0,
+            2: 0,
+            1: 0,
+        }
+        for (var i = 0; i < testArray.length; i++) {
+
+            object[testArray[i]] = object[testArray[i]] + 1
+            console.log(object)
+
+        }
+        var percArray = []
+        console.log('-----------------', Object.values(object))
+        for (var i = Object.values(object).length; i > 0; i--) {
+
+            var chartProgress;
+            chartProgress = (object[i] / testArray.length) * 100
+            percArray.push(chartProgress)
+            console.log('chartt', percArray)
+        }
+
+
+
+
         console.log(reviewData)
         console.log(ratingArray)
 
@@ -27,14 +61,14 @@ function StarChart({ reviewData }) {
 
             <div >
                 <p> {percentage} % of reviews recommended this product</p>
-                {ratingArray.map((rating) => {
+                {percArray.map((perc, index) => {
                     return (
                         <div className='grid grid-cols-6 gap-4 mt-2 mx-auto'>
-                            <div className=" w-16 underline"> {i--} stars</div>
+                            <div className=" w-16 underline"> {Math.abs(index - percArray.length)} stars</div>
                             <div className="col-span-5 ... ">
                                 <div className="relative pt-2.5  w-64">
                                     <div style={{ width: '100%' }} className="overflow-hidden  h-2 text-xs flex bg-gray-200 ">
-                                        <div style={{ width: '50%' }}
+                                        <div style={{ width: `${perc}%` }}
                                             className=" shadow flex flex-col text-center whitespace-nowrap text-white justify-center bg-green-600">
                                         </div>
                                     </div>
