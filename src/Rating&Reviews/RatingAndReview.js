6e03dfb40640650c8156b9ce43e7da02f8290e9d
Imad el Ayoubi <imadelayoubi07@gmail.com>
2021-10-18 13:16:01 +0100

    testing with different id

--- src/Rating&Reviews/RatingAndReview.js
+++ src/Rating&Reviews/RatingAndReview.js
@@ -25,7 +25,7 @@ class RatindAndReview extends React.Component {
 
 
   componentDidMount() {
-    axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews/?product_id=40344', {
+    axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews/?product_id=40393', {
       headers: {
         authorization:(process.env.REACT_APP_Token)
       }
