c8ad7cadc76c0fc87a1293a7b3f96414545610a7
Imad el Ayoubi <imadelayoubi07@gmail.com>
2021-10-18 12:37:39 +0100

    percentage and progress bar dynamically rendering

--- src/Product-List/Components/PopUp.js
+++ src/Product-List/Components/PopUp.js
@@ -4,20 +4,20 @@ import Comparison from './Comparison';
 
 
 export default function PopUp() {
-    
+
   const [open, setOpen] = useState(false);
   const closeModal = () => setOpen(false);
-    return (
-        <div >
-            <button type="button" className="relative button ml-4 p-20 border-2 border-transparent bg-white-500" onClick={(e) => { e.preventDefault(); setOpen(o => !o) }}>
-                      </button>
-                      <div className="pop"> 
-                  <Popup open={open} closeOnDocumentClick onClose={closeModal}>
-                 
-                    <Comparison/>
-                    
-                  </Popup>
-                  </div>
-        </div>
-    )
+  return (
+    <div >
+      <button type="button" className="relative button ml-4 p-20 border-2 border-transparent bg-white-500" onClick={(e) => { e.preventDefault(); setOpen(o => !o) }}>
+      </button>
+      <div className="pop ">
+        <Popup open={open} closeOnDocumentClick onClose={closeModal}>
+
+          <Comparison />  
+
+        </Popup>
+      </div>
+    </div>
+  )
 }
